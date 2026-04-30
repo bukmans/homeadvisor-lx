a226db74a0b7ec8f86e938690334c42a1d36d9c7
Always-Ascending <infotrac247@gmail.com>
2026-04-30 17:21:43 +0200

    Configure GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/homeadvisor-lx/',
   build: {
     outDir: 'dist',
     sourcemap: false,
